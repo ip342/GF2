277fe7dcd18a3eca98c265c6044d63bcf3e833e5
Ilakya Prabhakar <ilakya1@yahoo.com>
2020-05-13 15:43:52 +0100

    class initialiser method written

--- scanner.py
+++ scanner.py
@@ -52,11 +52,39 @@ class Scanner:
     def __init__(self, path, names):
         """Open specified file and initialise reserved words and IDs."""
 
+        # Open file
         try:
             self.input_file = open(path, 'r')
         except FileNotFoundError:
             raise FileNotFoundError('Error: No such file in current directory')
             sys.exit()
+            
+        # Remove blank lines, store file in list of each line
+        self.file_as_list = []
+        with self.input_file as file:
+            for line in file:
+                line = line.strip()
+                if line:
+                    self.file_as_list.append(line)
+        
+        # Initialise symbol types
+        self.names = names
+        self.symbol_list = [self.HEADER, self.PARAMETER, self.NAME, self.EQUALS, self.COMMA, self.OPEN_SQUARE, self.CLOSE_SQUARE, self.EQUALS, self.SEMICOLON, self.ARROW, self.DOT, self.OPEN_CURLY, self.CLOSE_CURLY, self.HASH, self.EOF]
+
+        self.header_list = ['DEVICES', 'CONNECTIONS', 'MONITOR']
+        [self.DEVICES_ID, self.CONNECTIONS_ID, self.MONITOR_ID] = self.names.lookup(self.header_list)
+
+        self.parameter_list = ['cycle', 'cycles', 'input', 'inputs']
+        [self.CYCLE, self.CYCLES, self.INPUT, self.INPUTS] = self.names.lookup(self.parameter_list)
+        
+        self.device = ['device']
+        [self.DEVICE] = self.names.lookup(self.device)
+        
+        self.end_symbols = [self.SEMICOLON, self.CLOSE_CURLY, self.CLOSE_SQUARE, self.EOF]
+
+        self.current_character = ''
+        self.current_line = 0
+        self.current_character_number = 0
 
     def get_symbol(self):
         """Translate the next sequence of characters into a symbol."""
