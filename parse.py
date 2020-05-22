532c13909712185a1796f65709b56e47540524d3
Ethan Tattersall <ejt68@cam.ac.uk>
2020-05-22 21:42:11 +0100

    Added option for gates to have start connection input port None

--- parse.py
+++ parse.py
@@ -192,6 +192,7 @@ class Parser:
                     I1, I2 = self.devices.names.lookup(["I1", "I2"])
                     self.devices.add_input(device_id, I1)
                     self.devices.add_input(device_id, I2)
+                    self.devices.add_input(device_id, None)
 
             elif self.symbol.id == self.devices.CLOCK:
 
@@ -285,6 +286,7 @@ class Parser:
                                 ["I"+str(input_num)])
 
                             self.devices.add_input(device_id, input_id)
+                        self.devices.add_input(device_id, None)
 
         self.symbol = self.scanner.get_symbol()
 
@@ -513,7 +515,6 @@ class Parser:
                 SemanticError, "Invalid port index.")
             return True
         elif con_status == self.network.NO_ERROR:
-            print('NO ERROR')
             pass
 
         # CHECK for semicolon
