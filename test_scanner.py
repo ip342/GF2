ae113234b07661ab015c6c942e9023b0a28ebf93
Ilakya Prabhakar <ilakya1@yahoo.com>
2020-05-23 14:08:28 +0100

    error function now raises error

--- test_scanner.py
+++ test_scanner.py
@@ -24,20 +24,24 @@ def test_scanner():
     return scan
 
 @pytest.fixture
+def lines():
+    return ['     d3 2342 inputs, ->', ' #l2534', ' 5..']
+
+@pytest.fixture
 def no_spaces():
-    return 'd32342inputs,->l2534..'
+    return 'd32342inputs,->#l25345..'
 
 
-def file_not_found(test_names):
+def test_file_not_found(test_names):
     """Test that a file not found error works"""
     with pytest.raises(FileNotFoundError):
         Scanner("fakefile.txt", test_names)
 
 
-def file_as_list(test_scanner, test_names, second_line=' l2534', third_line=' 5..')
-    """Test that the file as list function works """
-    test_scanner.file_as_list[1] == second_line
-    test_scanner.file_as_list[2] == third_line
+def test_file_as_list(test_scanner, test_names):
+    """Test the file as list function works """
+    test_scanner.file_as_list == lines
+
 
 def test_skip_spaces(test_scanner, test_names, first_char="d"):
     """Test the self.skip_spaces() function of the scanner"""
@@ -46,14 +50,15 @@ def test_skip_spaces(test_scanner, test_names, first_char="d"):
 
 
 def test_advance(test_scanner, no_spaces):
-    """Test the self.advance() functionality of the scanner class"""
+    """Test the self.advance() function of the scanner"""
     i = 0
-    while i <= len(no_spaces)-1:
-        expected = no_spaces[i]
+    for i in range(len(no_spaces)-1):
+        current_char = no_spaces[i]
         test_scanner.skip_spaces()
-        assert test_scanner.current_character == expected
+        assert test_scanner.current_character == current_char
         test_scanner.advance()
         i += 1
+
 
 def test_get_name_and_number(test_scanner, test_names):
     """check that the get_name function gives out a valid name and the next character,
@@ -62,8 +67,14 @@ def test_get_name_and_number(test_scanner, test_names):
     name = test_scanner.get_name()
     assert name[0] == "d3"
     assert name[1] == " "
-    assert name[0].isalnum()
     test_scanner.advance()
     number = test_scanner.get_number()
     assert number[0] == "2342"
     assert number[1] == " "
+
+def test_raise_comment_error(test_scanner, test_names):
+    
+    with pytest.raises(SyntaxError):
+        while test_scanner.current_character != len():
+            test_scanner.get_symbol()
+            i+=1
